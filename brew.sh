9328e523b5e4f93b7eab3eabbc491f96251ca372
Au <>
2022-08-05 16:13:25 +0800

    init

---
+++ brew.sh
@@ -0,0 +1,9 @@
+# 切换为阿里源
+git -C "$(brew --repo)" remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
+git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
+echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.zshrc
+source ~/.zshrc
+brew update
+
+# 赋予文件夹权限
+sudo chown -R $(whoami) /usr/local/var/homebrew
