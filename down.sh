2fe92870946505c00bd65138faa7bafdb5ed68ae
Au <1570967070@qq.com>
2022-11-07 17:06:19 +0800

    Create down.sh

---
+++ down.sh
@@ -0,0 +1,5 @@
+cd /bin;
+curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/start.sh;
+curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/stop.sh;
+curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/restart.sh;
+chmod +x start.sh stop.sh restart.sh;
