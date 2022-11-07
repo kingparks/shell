82e4025e8496a9297ee7be1475e38daa93ec6432
Au <1570967070@qq.com>
2022-11-07 16:29:18 +0800

    start.sh
    ${script_dir}

---
+++ start.sh
@@ -0,0 +1,8 @@
+# 启动执行目录下的某个应用 例如：cd xxx;start.sh test
+app=$1
+echo "start $app"
+# 脚本目录
+#script_dir=$(cd "$(dirname $0)";pwd)/
+# 执行目录
+script_dir=$(pwd)/
+nohup ${script_dir}${app} > ${script_dir}${app}.out 2>&1 &
