087a9f61505ac33309cf9f544f54cab8f637093d
Au <1570967070@qq.com>
2022-11-07 16:13:58 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,8 +1,10 @@
-# 停止当前目录下的某个应用 例如：./stop.sh test
+# 停止执行目录下的某个应用 例如：stop.sh test
 app=$1
 echo "stop $app"
-# 当前目录
-script_dir=$(cd "$(dirname $0)";pwd)/
+# 脚本目录
+#script_dir=$(cd "$(dirname $0)";pwd)/
+# 执行目录
+script_dir=$(pwd)/
 pids=`ps -ef | grep $app | grep -v 'cd'| grep -v 'grep' | awk '{print $2}' | while read line; do pwdx $line; done`
 pids=`echo "$pids" | awk '{print $0"/"}'`
 echo "$pids" | grep "$script_dir" | cut -d':' -f1 | while read line; do kill $line; done;
