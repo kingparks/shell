08cea7d97d19ef5b2e6467b82eeae7d73dcf3d5c
Au <1570967070@qq.com>
2022-11-08 16:04:22 +0800

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,4 +1,4 @@
-cd /bin;
+cd /usr/local/bin;
 curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/start.sh;
 curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/stop.sh;
 curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/restart.sh;
