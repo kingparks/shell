95d3e7ee9bb15e901a0150935f6f7c289ad56a84
Au <1570967070@qq.com>
2022-11-08 17:45:35 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -9,7 +9,7 @@ script_dir=$(pwd)/
 function pwdx2 {
   echo $1: `lsof -a -p $1 -d cwd -n | tail -1 | awk '{print $NF}'`
 }
-pids=`ps -ef | grep $app | grep -v 'cd'| grep -v 'grep' | grep -v 'tail' | awk '{print $2}' | while read line; do pwdx2 $line; done`
+pids=`ps -ef | grep $app | grep -v 'cd\|grep\|tail\|echo\|stop.sh'| awk '{print $2}' | while read line; do pwdx2 $line; done`
 # 在后面拼接 /
 pids=`echo "$pids" | awk '{print $0"/"}'`
 echo "$pids" | grep "$script_dir" | cut -d':' -f1 | while read line; do kill $line; done;
