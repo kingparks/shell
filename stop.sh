84ff331498f90da39ab88924157ec0cf0da1208d
Au <1570967070@qq.com>
2022-11-08 15:49:57 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -5,6 +5,7 @@ echo "stop $app"
 #script_dir=$(cd "$(dirname $0)";pwd)/
 # 执行目录
 script_dir=$(pwd)/
+# 获取 pid 及程序目录 eg:[11: /service/gfast]
 pids=`ps -ef | grep $app | grep -v 'cd'| grep -v 'grep' | grep -v 'tail' | awk '{print $2}' | while read line; do pwdx $line; done`
 pids=`echo "$pids" | awk '{print $0"/"}'`
 echo "$pids" | grep "$script_dir" | cut -d':' -f1 | while read line; do kill $line; done;
