9a80044f1fdabd271d9201d7bca21be5f180957c
Au <1570967070@qq.com>
2022-11-08 16:27:40 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -7,5 +7,6 @@ echo "stop $app"
 script_dir=$(pwd)/
 # 获取 pid 及程序目录 eg:[11: /service/gfast]
 pids=`ps -ef | grep $app | grep -v 'cd'| grep -v 'grep' | grep -v 'tail' | awk '{print $2}' | while read line; do pwdx $line; done`
+# 在后面拼接 /
 pids=`echo "$pids" | awk '{print $0"/"}'`
 echo "$pids" | grep "$script_dir" | cut -d':' -f1 | while read line; do kill $line; done;
