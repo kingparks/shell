4367ca979fb2c11fe3214460c86f23148a597c8a
Au <1570967070@qq.com>
2022-11-20 13:33:46 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,4 +1,4 @@
-# brew切换为阿里源
+# brew 切换为阿里源
 git -C "$(brew --repo)" remote set-url origin https://mirrors.aliyun.com/homebrew/brew.git
 git -C "$(brew --repo homebrew/core)" remote set-url origin https://mirrors.aliyun.com/homebrew/homebrew-core.git
 echo 'export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles' >> ~/.zshrc
