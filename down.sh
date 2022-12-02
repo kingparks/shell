ac01e055f30118c639aa4756fc943745d46b0ea8
Au <1570967070@qq.com>
2022-12-02 10:13:13 +0800

    下载脚本并放置运行路径

--- down.sh
+++ down.sh
@@ -1,6 +1,8 @@
-# 下载通用运行/停止/重启脚本
-cd /usr/local/bin;
-curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/start.sh;
-curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/stop.sh;
-curl -O https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/shell/main/restart.sh;
-chmod +x start.sh stop.sh restart.sh;
+# 下载脚本并放置运行路径
+git clone https://ghproxy.com/https://github.com/Jetereting/shell.git tmp_git_shell &&
+cd tmp_git_shell;
+shells=`ls | grep .sh | tr "\n" " "`;
+chmod +x `echo $shells`;
+sudo mv `echo $shells` /usr/local/bin;
+rm -rf ../tmp_git_shell;
+echo "完成！"
