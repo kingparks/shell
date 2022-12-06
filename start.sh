ed06312a0be85577a34cba8c377de84cb14322f5
Au <1570967070@qq.com>
2022-12-06 15:05:26 +0800

    通用启动脚本

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
-# 启动执行目录下的某个应用 例如：cd xxx;start.sh test 
+# 通用启动脚本
+# 启动执行目录下的某个应用 例如：cd xxx;start.sh test '-c conf/conf.yaml'
 app=$1
 echo "start $app"
 # 脚本目录
 #script_dir=$(cd "$(dirname $0)";pwd)/
 # 执行目录
 script_dir=$(pwd)/
-nohup ${script_dir}${app} > ${script_dir}${app}.out 2>&1 &
+nohup ${script_dir}${app} $2 > ${script_dir}${app}.out 2>&1 &
