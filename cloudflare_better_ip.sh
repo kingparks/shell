196770311fc99a1290899f9660fee35ac8293bab
Au <1570967070@qq.com>
2022-12-06 14:32:16 +0800

    cloudflare ipv4 优选

--- cloudflare_better_ip.sh
+++ cloudflare_better_ip.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # cloudflare ipv4 优选
 curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/badafans/better-cloudflare-ip/master/shell/cf.sh > cf.sh
-sed -i '' 's/read -p "请选择菜单(默认0): " menu/menu=1/' cf.sh
-sed -i '' 's/read -p "请设置期望的带宽大小(默认最小1,单位 Mbps):" bandwidth/bandwidth=1/' cf.sh
-sed -i '' 's/read -p "请设置RTT测试进程数(默认10,最大50):" tasknum/tasknum=10/' cf.sh
-sed -i '' 's/ips-v4.txt/tmp_ips-v4.txt/' cf.sh
-sed -i '' 's/ips-v6.txt/tmp_ips-v6.txt/' cf.sh
-sed -i '' 's/colo.txt/tmp_colo.txt/' cf.sh
-sed -i '' 's/url.txt/tmp_url.txt/' cf.sh
+sed -i.bak 's/read -p "请选择菜单(默认0): " menu/menu=1/' cf.sh
+sed -i.bak 's/read -p "请设置期望的带宽大小(默认最小1,单位 Mbps):" bandwidth/bandwidth=1/' cf.sh
+sed -i.bak 's/read -p "请设置RTT测试进程数(默认10,最大50):" tasknum/tasknum=10/' cf.sh
+sed -i.bak 's/ips-v4.txt/tmp_ips-v4.txt/' cf.sh
+sed -i.bak 's/ips-v6.txt/tmp_ips-v6.txt/' cf.sh
+sed -i.bak 's/colo.txt/tmp_colo.txt/' cf.sh
+sed -i.bak 's/url.txt/tmp_url.txt/' cf.sh
 bash cf.sh
-rm cf.sh tmp_ips-v4.txt tmp_ips-v6.txt tmp_colo.txt tmp_url.txt
+rm cf.sh cf.sh.bak tmp_ips-v4.txt tmp_ips-v6.txt tmp_colo.txt tmp_url.txt
