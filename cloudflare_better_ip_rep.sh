e62881ece7768a98c62b72a45544b71b613567f6
Au <1570967070@qq.com>
2022-12-06 14:33:12 +0800

    检测并自动替换 cloudflare 下域名的 hosts ip

--- cloudflare_better_ip_rep.sh
+++ cloudflare_better_ip_rep.sh
@@ -17,16 +17,16 @@ if [ -z "$oldIP" ]; then
 fi
 
 curl -fsSL https://ghproxy.com/https://raw.githubusercontent.com/badafans/better-cloudflare-ip/master/shell/cf.sh >cf.sh
-sed -i '' 's/read -p "请选择菜单(默认0): " menu/menu=1/' cf.sh
-sed -i '' 's/read -p "请设置期望的带宽大小(默认最小1,单位 Mbps):" bandwidth/bandwidth=1/' cf.sh
-sed -i '' 's/read -p "请设置RTT测试进程数(默认10,最大50):" tasknum/tasknum=10/' cf.sh
-sed -i '' 's/ips-v4.txt/tmp_ips-v4.txt/' cf.sh
-sed -i '' 's/ips-v6.txt/tmp_ips-v6.txt/' cf.sh
-sed -i '' 's/colo.txt/tmp_colo.txt/' cf.sh
-sed -i '' 's/url.txt/tmp_url.txt/' cf.sh
+sed -i.bak 's/read -p "请选择菜单(默认0): " menu/menu=1/' cf.sh
+sed -i.bak 's/read -p "请设置期望的带宽大小(默认最小1,单位 Mbps):" bandwidth/bandwidth=1/' cf.sh
+sed -i.bak 's/read -p "请设置RTT测试进程数(默认10,最大50):" tasknum/tasknum=10/' cf.sh
+sed -i.bak 's/ips-v4.txt/tmp_ips-v4.txt/' cf.sh
+sed -i.bak 's/ips-v6.txt/tmp_ips-v6.txt/' cf.sh
+sed -i.bak 's/colo.txt/tmp_colo.txt/' cf.sh
+sed -i.bak 's/url.txt/tmp_url.txt/' cf.sh
 echo '执行中请等待...'
 bash cf.sh >tmp_cf_result.txt
-rm cf.sh tmp_ips-v4.txt tmp_ips-v6.txt tmp_url.txt tmp_colo.txt
+rm cf.sh cf.sh.bak tmp_ips-v4.txt tmp_ips-v6.txt tmp_url.txt tmp_colo.txt
 newIP=$(cat tmp_cf_result.txt | grep '优选IP' | cut -d' ' -f2)
 rm tmp_cf_result.txt
 if [ -z "$newIP" ]; then
@@ -35,5 +35,5 @@ if [ -z "$newIP" ]; then
 fi
 
 echo 'hosts 文件的 ip 由' $oldIP '变为' $newIP
-sudo sed -i "" "s/$oldIP/$newIP/g" /etc/hosts
+sudo sed -i.bak "s/$oldIP/$newIP/g" /etc/hosts
 
