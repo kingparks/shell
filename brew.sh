30689d31da75d10726973ca61369e2b18c0ff160
Au <1570967070@qq.com>
2022-12-23 17:12:08 +0800

    brew 切换为中国源

--- brew.sh
+++ brew.sh
@@ -1,3 +1,9 @@
 # brew 切换为中国源 
 
 sudo bash -c "$(curl -fsSL 	https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
+sudo chown -R $(whoami) /Users/au/Library/Caches/Homebrew
+exit;
+brew update-reset;
+git config --global --add safe.directory /opt/homebrew/Library/Taps/homebrew/homebrew-cask;
+git config --global --add safe.directory /opt/homebrew/Library/Taps/homebrew/homebrew-core;
+
