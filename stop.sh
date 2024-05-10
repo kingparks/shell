d55a26b840e4bebd4c820c2536374e86004ca5bd
{1570967070@qq.com} <1570967070@qq.com>
2024-05-10 15:31:43 +0800

    init

--- stop.sh
+++ stop.sh
@@ -9,7 +9,11 @@ script_dir=$(pwd)/
 function pwdx2 {
   echo $1: `lsof -a -p $1 -d cwd -n | tail -1 | awk '{print $NF}'`
 }
-pids=`ps -ef | grep $1 | grep -v 'cd\|grep\|tail\|echo\|stop.sh'| awk '{print $2}' | while read line; do pwdx2 $line; done`
+if command -v pwdx >/dev/null 2>&1; then
+    pids=`ps -ef | grep $1 | grep -v 'cd\|grep\|tail\|echo\|stop.sh'| awk '{print $2}' | while read line; do pwdx $line; done`
+else
+    pids=`ps -ef | grep $1 | grep -v 'cd\|grep\|tail\|echo\|stop.sh'| awk '{print $2}' | while read line; do pwdx2 $line; done`
+fi
 # 在后面拼接 /
 pids=`echo "$pids" | awk '{print $0"/"}'`
 echo "$pids" | grep -e "${script_dir}$" | cut -d':' -f1 | while read line; do kill $line; done;
