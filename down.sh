cc8e51fbe15aab82cd5b335174ee6c3830bd3896
kingparks <137748916+kingparks@users.noreply.github.com>
2024-09-26 18:13:47 +0800

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,6 +1,6 @@
 # 下载脚本并放置运行路径
 set -e;
-curl -o tmp_git_shell.zip https://mirror.ghproxy.com/https://github.com/kingparks/shell/archive/main.zip
+curl -o tmp_git_shell.zip https://ghp.ci/https://github.com/kingparks/shell/archive/main.zip
 unzip -o -d tmp_git_shell  tmp_git_shell.zip
 cd tmp_git_shell/shell-main
 shells=`ls | grep .sh | tr "\n" " "`;
