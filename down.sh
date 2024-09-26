7bb8a6c336b82b38253562257b2a1860137f3a5f
kingparks <137748916+kingparks@users.noreply.github.com>
2024-09-26 18:20:31 +0800

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,6 +1,6 @@
 # 下载脚本并放置运行路径
 set -e;
-curl -o tmp_git_shell.zip https://ghp.ci/https://github.com/kingparks/shell/archive/main.zip
+curl -o tmp_git_shell.zip https://codeload.github.com/kingparks/shell/zip/refs/heads/main
 unzip -o -d tmp_git_shell  tmp_git_shell.zip
 cd tmp_git_shell/shell-main
 shells=`ls | grep .sh | tr "\n" " "`;
